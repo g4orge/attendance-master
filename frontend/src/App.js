0fd4d6f88cc7340245ebcd0387877ae98ce6dcbd
emmivvi <emdechene29@gmail.com>
2024-11-21 15:59:18 -0500

    added babel to jasonpackage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 //import Layout from './Components/Layout';
 import HomePage from './Pages/HomePage';
 import MainCalendar from './Pages/MainCalendar';
